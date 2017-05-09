f3644f0b895c89fae8fd18f3dbf746cfedd41755
daihuaxiehuakai <842159717@qq.com>
2017-05-09 08:44:06 +0800

    增加技术沙龙条目

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ $(function(){
 
 	var lists = [
 	{title:'实用网站收集',image:'images/website.png',des:'这里收集了开发过程中遇到的实用网站和一些有趣的网站'},
-	{title:'实用网站收集',image:'images/website.png',des:'这里收集了开发过程中遇到的实用网站和一些有趣的网站'},
+	{title:'技术沙龙',image:'images/1.jpg',des:'分享一些自己看到的技术文章，丰富自己的阅历。很多东西不一定要会但一定要有所了解。'},
 	{title:'实用网站收集',image:'images/website.png',des:'这里收集了开发过程中遇到的实用网站和一些有趣的网站'},
 	{title:'实用网站收集',image:'images/website.png',des:'这里收集了开发过程中遇到的实用网站和一些有趣的网站'},
 	{title:'实用网站收集',image:'images/website.png',des:'这里收集了开发过程中遇到的实用网站和一些有趣的网站'},
